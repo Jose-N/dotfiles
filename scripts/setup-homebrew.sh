cea4dbfcbe8d4f39a674dd1df99c3038a6b91e10
Jose Naylor <jose.c.naylor@gmail.com>
2026-02-24 00:22:14 -0500

    created shell script to setup homebrew and packages

---
+++ scripts/setup-homebrew.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Install homebrew if needed
+if command -v -- homebrew &> /dev/null; then 
+  echo "homebrew is already installed"
+else
+  echo "installing homebrew"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+fi
+
+Formulae=('jq' 'yq' 'bat' 'eza' 'fd' 'fzf' 'ripgrep' 'lazygit' 'lazydocker' 'lazysql' 'docker' 'docker-compose' 'powerlevel10k' 'tmux' 'neovim' 'zsh-autocomplete' 'zsh-autosuggestions' 'zsh-syntax-highlighting' 'try-rs' 'mise' 'zoxide')
+Casks=('1password' 'zen' 'font-hack-nerd-font' 'ghostty')
+
+for i in "${Formulae[@]}"; do
+  brew install "$i"
+done
+
+for i in "${Casks[@]}"; do
+  brew install "$i"
+done
+
