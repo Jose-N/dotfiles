dbd7b2a269ad9d271418f3dd93c088bc8f6ec601
Jose Naylor <jose.c.naylor@gmail.com>
2026-02-24 00:43:46 -0500

    added raycast to casks

--- scripts/setup-homebrew.sh
+++ scripts/setup-homebrew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install homebrew if needed
-if command -v -- homebrew &> /dev/null; then 
+if command -v -- homebrew &> /dev/null; then
   echo "homebrew is already installed"
 else
   echo "installing homebrew"
@@ -9,7 +9,7 @@ else
 fi
 
 Formulae=('jq' 'yq' 'bat' 'eza' 'fd' 'fzf' 'ripgrep' 'lazygit' 'lazydocker' 'lazysql' 'docker' 'docker-compose' 'powerlevel10k' 'tmux' 'neovim' 'zsh-autocomplete' 'zsh-autosuggestions' 'zsh-syntax-highlighting' 'try-rs' 'mise' 'zoxide')
-Casks=('1password' 'zen' 'font-hack-nerd-font' 'ghostty')
+Casks=('1password' 'zen' 'font-hack-nerd-font' 'ghostty' 'raycast')
 
 for i in "${Formulae[@]}"; do
   brew install "$i"
