bbc3579db135a63d56cddd34f4090c92dc77d4d8
Jose Naylor <jose.naylor@finxact.com>
2026-03-05 16:21:39 -0500

    added mise config for languages i use, setup zsh to use mise, added script to put dotfiles to proper place on system

--- .zshrc
+++ .zshrc
@@ -65,3 +65,5 @@ source /opt/homebrew/share/powerlevel10k/powerlevel10k.zsh-theme
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+eval "$(mise activate zsh)"
--- scripts/setup-dotfiles.sh
+++ scripts/setup-dotfiles.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if ! [[ -d ~/.config ]]; then
- echo "creating .config dir"
- mkdir ~/.config
+  echo "creating .config dir"
+  mkdir ~/.config
 fi
 
-cp ../.p10k.zsh ~
-cp ../.zshrc ~
-cp -r ../* ~/.config
+cp ~/code/dotfiles/.p10k.zsh ~
+cp ~/code/dotfiles/.zshrc ~
+cp -r ~/code/dotfiles/* ~/.config
