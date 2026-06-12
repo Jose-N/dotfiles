7c30aefa3b04e4d9b040ab50b92a6246d9cfcb2c
Jose Naylor <jose.naylor@finxact.com>
2026-06-12 12:14:54 -0400

    added azure-cli to homebrew list

--- scripts/setup-homebrew.sh
+++ scripts/setup-homebrew.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Install homebrew if needed
-if command -v -- homebrew &> /dev/null; then
+if command -v -- homebrew &>/dev/null; then
   echo "homebrew is already installed"
 else
   echo "installing homebrew"
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-Formulae=('jq' 'yq' 'bat' 'eza' 'fd' 'fzf' 'ripgrep' 'lazygit' 'lazydocker' 'lazysql' 'docker' 'docker-compose' 'powerlevel10k' 'tmux' 'neovim' 'zsh-autocomplete' 'zsh-autosuggestions' 'zsh-syntax-highlighting' 'try-rs' 'mise' 'zoxide')
+Formulae=('jq' 'yq' 'bat' 'eza' 'fd' 'fzf' 'ripgrep' 'lazygit' 'lazydocker' 'lazysql' 'docker' 'docker-compose' 'powerlevel10k' 'tmux' 'neovim' 'zsh-autocomplete' 'zsh-autosuggestions' 'zsh-syntax-highlighting' 'try-rs' 'mise' 'zoxide' 'azure-cli')
 Casks=('1password' 'zen' 'font-hack-nerd-font' 'ghostty' 'raycast')
 
 for i in "${Formulae[@]}"; do
@@ -18,4 +18,3 @@ done
 for i in "${Casks[@]}"; do
   brew install "$i"
 done
-
