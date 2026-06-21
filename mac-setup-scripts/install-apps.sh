4a14fcef36b5e5fab577e82dec714a8fb40833cb
test <test@tests-Virtual-Machine.local>
2026-06-20 21:42:16 -0400

    seperated homebrew from app installs, points doom doctor to absolute location

---
+++ mac-setup-scripts/install-apps.sh
@@ -0,0 +1,1 @@
+./homebrew/install-formulae.sh ./homebrew/install-nerd-front.sh && ./homebrew/install-emacs.sh && ./homebrew/install-1password.sh && ./homebrew/install-raycast.sh && ./homebrew/install-zen.sh
