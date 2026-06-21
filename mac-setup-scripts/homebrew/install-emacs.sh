d3a5ceb87d186686810390b7dec4674e2796ef4d
test <10716409+Jose-N@users.noreply.github.com>
2026-06-21 17:06:59 -0400

    Added feh to deps soI can use the wallpaper package

--- mac-setup-scripts/homebrew/install-emacs.sh
+++ mac-setup-scripts/homebrew/install-emacs.sh
@@ -2,7 +2,7 @@
 
 # Install dependencies
 figlet "Installing Emacs Dependencies" | lolcat
-Formulae=('ripgrep' 'fd' 'coreutils' 'fontconfig' 'cmake' 'shellcheck' 'mu' 'offlineimap' 'imagemagick')
+Formulae=('ripgrep' 'fd' 'coreutils' 'fontconfig' 'cmake' 'shellcheck' 'mu' 'offlineimap' 'imagemagick' 'feh')
 
 for i in "${Formulae[@]}"; do
   brew install "$i"
